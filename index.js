bdc72d5fa18f8e5057b2ae75a26ffa7b79e70c10
NAWF ABDULLAH <92883149+NawfAbdullah@users.noreply.github.com>
2022-12-21 19:18:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function navSlide() {
+    var burger = document.querySelector('.burger');
+    var nav = document.querySelector('.nav-link');
+   
+  
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+  
+     
+       //burger animation 
+       burger.classList.toggle('toggle');
+  
+    });
+  
+  
+  
+  }
+  
+  navSlide()
+  $(document).ready(function(){
+    buildMap();
+  });
+  
+ 
